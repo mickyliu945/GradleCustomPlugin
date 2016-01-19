0d2c34b239d3fe446b6596827d310b9ad5f4ee57
mickyliu <mickyliu@126.com>
2016-01-19 13:33:53 +0800

    first commit

---
+++ CustomPlugin/plugin/src/main/groovy/com/micky/gradle/MyCustomPlugin.groovy
@@ -0,0 +1,11 @@
+package com.micky.gradle;
+
+import org.gradle.api.*;
+
+class MyCustomPlugin implements Plugin<Project> {
+	void apply(Project project) {
+		project.task('myTask') << {
+			println "Hi this is micky's plugin"
+		}
+	}
+}
